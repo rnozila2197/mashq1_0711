25c76847cd69f1b7a4d1e274cba482033aa474a0
rnozila2197 <yjamshid2197@gmail.com>
2025-11-07 17:59:37 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+#1
+for i in range(1, 50):
+    if i % 7 == 0:
+        print(i)
+        break
+
+#2
+for i in range(100, 150):
+    if i == 137:
+        print("Parol topildi!")
+        break
+
+#3
+sum = 0
+for i in range(1, 21):
+        sum += i
+        if sum >= 20:
+            break
+
+print(i)
+
+#4
+kop = 0
+for i in range(1, 21):
+    kop += i
+    if i >= 10000 == 0:
+        break
+
+print(kop)
